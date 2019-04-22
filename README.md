e609de7e2c87a415ffb1de12776bccd9980d2d66
lukaslukas <eiglerhardingl@gmail.com>
2019-04-22 19:56:26 -0400

    updated w most recent info waiting for WW for launch

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # arch-temp
+
+
+## To Do
+- [x] double check brown color
+- [x] changes times to helvetica + type hierarchies
+- [x] shorter text, wait for content? 
+- [x] add register link
+- [x] image credit locations
+- [x] position: fixed marquee
+- [x] building of the day call + image
+- [x] emphasize preview in banner with leading sentence
+
+
+## questions: 
+- [ ] noguchi doesn't ahve a registration link yet? 
--- assets/script.js
+++ assets/script.js
@@ -3,65 +3,83 @@ var Site = {};
 Site.buildings = [
 	{
 		title: "BRIC Arts | Media House",
-		time: "Monday, May 13,<br>12:00 – 1:00pm",
-		architect: "Leeser Architecture",
+		time: "Monday, May 13<br>12:00 – 1:00pm",
+		architect: "Thomas W. Lamb (1918); renovation by Leeser Architecture",
 		image: "images/BRIC_Exterior_temp.jpg",
+		caption: "Image Credit: Courtesy of Leeser Architecture",
 		link: "https://archtober.org",
 		style:{ 
 			size: "small",
 			color: "#C028B9"
 		},
-		description: "The project is a community arts center for two arts organizations. While pursuing wildly different types of work, the mission of each organization is centered around community outreach and participation. The primary project objective is the expansion of BRIC and UrbanGlass to the vacant ground floor and to visually open the building to the street. Leeser Architecture juggled complex programmatic demands and highly specific uses to create an open and accessible arts facility that can connect the community as well as invite them inside to engage with the myriad of activities occurring within.</p><p>Leeser Architecture’s renovation and expansion of this existing, under-utilized building involves two user groups, BRIC Arts | Media | Brooklyn and UrbanGlass. The project includes a full renovation of the interior space to create new theaters, television broadcasting studios, art galleries, glass workshops, media labs, classrooms, administrative and operation offices, and support spaces."
+		description: "Leeser Architecture’s renovation and expansion of an existing, under-utilized building involves two user groups: BRIC Arts | Media | Brooklyn and UrbanGlass. Leeser Architecture juggled complex programmatic demands and highly specific uses to create an open and accessible arts facility that invites the community inside to engage with the myriad of activities occurring within.The project includes a full renovation of the interior space to create new theaters, television broadcasting studios, art galleries, glass workshops, media labs, classrooms, administrative and operation offices, and support spaces.",
+		registration: "https://aiany.secure.force.com/pmtx/evt__QuickEvent?id=a0b4N00002CIVxT"
 	},
 	{
 		title: "Solomon R. Guggenheim Museum",
-		time: "Tuesday, May 14,<br>6:00 – 7:00pm",
+		time: "Tuesday, May 14<br>6:00 – 7:00pm",
 		architect: "Frank Lloyd Wright (1959)",
 		image: "images/guggenheim.jpg",
+		caption: "Image Credit: William Short. © Solomon R. Guggenheim Foundation, New York. All Rights Reserved.",
 		link: "https://archtober.org",
 		style:{ 
 			size: "medium",
 			color: "#FF4438"
 		},
-		description: "An in-depth look at the design, construction and history of the iconic Frank Lloyd Wright-designed building. Ashley Mendelsohn, Assistant Curator of Architecture and Digital Initiatives will give a tour that focuses on how the structure challenges curators, artists and exhibitions designers to reinvent the space anew for each exhibition."
+		description: "An in-depth look at the design, construction and history of the iconic Frank Lloyd Wright-designed building. Ashley Mendelsohn, Assistant Curator of Architecture and Digital Initiatives, will give a tour that focuses on how the structure challenges curators, artists, and designers to reinvent the space anew for each exhibition.",
+		registration: "https://aiany.secure.force.com/pmtx/evt__QuickEvent?id=a0b4N00002CIVxY"
 	},
 	{
 		title: "Staten Island Lighthouse Museum",
-		time: "Wednesday, May 15,<br>6:00 – 7:00pm",
-		architect: "(Architect TBD)",
+		time: "Wednesday, May 15<br>6:00 – 7:00pm",
+		architect: "Alfred B. Mullett (1871)",
 		image: "images/DJI_0046.JPG",
+		caption: "Image Credit: Courtesy of the Staten Island Lighthouse Museum",
 		link: "https://archtober.org",
 		style:{ 
 			size: "small",
 			color: "#0082CA"
 		},
-		description: "A three minute walk from the Staten Island Ferry, the National Lighthouse Museum site tour explores the history of the seven remaining buildings from the United States Lighthouse Establishment era (1864-1939). Built for the Treasury Department of the US Government, these buildings housed lampists, blacksmiths, clerks, engineers, and inspectors that helped supply the nation's light stations. This tour will also cover how lighthouse engineering and design changed over time. Free Museum admission is included."
+		description: "A three-minute walk from the Staten Island Ferry, the National Lighthouse Museum tour will explore the history of the seven remaining buildings from the United States Lighthouse Establishment era (1864-1939). Built for the Treasury Department of the US Government, these buildings housed lampists, blacksmiths, clerks, engineers, and inspectors that helped supply the nation's light stations. This tour will also cover how lighthouse engineering and design changed over time.",
+		registration: "https://aiany.secure.force.com/pmtx/evt__QuickEvent?id=a0b4N00002CIVxi"
 	},
 	{
 		title: "Noguchi Museum Renovation",
-		time: "Thursday, May 16,<br>12:00 – 1:00pm",
+		time: "Thursday, May 16<br>12:00 – 1:00pm",
 		architect: "Isamu Noguchi (1985); renovation by Sage and Coombe Architects",
 		image: "images/3-The-Noguchi-Museum-New-York-Area-14--photo-Nicholas-Knight.jpg",
+		caption: "Image Credit: Nicholas Knight",
 		link: "https://archtober.org",
 		style:{ 
 			size: "large",
 			color: "#FF5100"
 		},
-		description: "Sage and Coombe worked with the Noguchi Museum in Long Island City, Queens for more than a decade to renovate and modernize the historic building in three phases, affording the Museum accessibility, allowing it to remain open year-round, and bringing it to the rigorous standards of contemporary museums – all while maintaining the unique character of the space. The renovation encompassed all areas of the Museum building and garden, with construction on the final phase completed in Spring 2016."
+		description: "Sage and Coombe worked with the Noguchi Museum in Long Island City, Queens for more than a decade to renovate and modernize the historic building in three phases, affording the Museum accessibility, allowing it to remain open year-round, and bringing it to the rigorous standards of contemporary museums – all while maintaining the unique character of the space. The renovation encompassed all areas of the Museum building and garden, with construction on the final phase completed in Spring 2016.",
+		registration: "https://aiany.secure.force.com/pmtx/evt__QuickEvent?id=a0b4N00002CIVxn"
 	},
 	{
-		title: "Bronx River House",
-		time: "Friday, May 17,<br>1:00 – 2:00pm",
-		architect: "Kiss+Cathcart Architects",
+		title: "Bronx River Greenway River House",
+		time: "Friday, May 17<br>11:00 – 12:00pm",
+		architect: "Kiss + Cathcart",
 		image: "images/Construction_Image.jpg",
+		caption: "Image Credit: Kiss + Cathcart",
 		link: "https://archtober.org",
 		style:{ 
 			size: "medium",
 			color: "#789904"
 		},
-		description: "The Bronx River Greenway River House, nearing completion, is the base of operations for the restoration and operation of the Bronx River Greenway and the adjacent portion of the Bronx River. The Bronx River Alliance will occupy the building and manage it on behalf of the Park and public and community groups. The Bronx River House will approach net-zero energy and will use rainwater-irrigated living facades to create a favorable microclimate. The project received a Design Award from the Art Commission of the City of New York."
+		description: "The Bronx River Greenway River House, nearing completion, is the base of operations for the Bronx River Greenway and the adjacent portion of the Bronx River. The Bronx River Alliance will occupy the building and manage it on behalf of the park, the public, and community groups. The Bronx River House will approach net-zero energy and will use rainwater-irrigated living facades to create a favorable microclimate. The project received a Design Award from the Art Commission of the City of New York.",
+		registration: "https://aiany.secure.force.com/pmtx/evt__QuickEvent?id=a0b4N00002CIVxx"
 	}
 ]
+
+Site.callforbuidling = {
+	title: "Call for Archtober Buildings of the Day",
+	deadline: "Deadline: Friday, May 17",
+	image: "images/archtober_building-of-the-day.jpg",
+	description: "This Archtober, share your firm’s New York City-based works with architecture enthusiasts! Archtober is accepting nominations for projects to be included in New York City’s month-long festival of architecture and design.</p><p>Building of the Day tours are the centerpiece of Archtober. With the goal of making exemplary architecture and the work of architects more accessible to the general public, the lunchtime tours, led by project architects, generate international publicity and provide a platform for engagement in important issues in the New York City built environment.",
+	link: "https://www.centerforarchitecture.org/news/call-for-archtober-buildings-of-the-day-2/"
+}
 
 Site.appendInfo = function(){
 
@@ -70,16 +88,31 @@ Site.appendInfo = function(){
 		$("#arch_preview").append(`<li class="preview_building ${building.style.size}">
 				<h5>NYCxDesign Building of the Day Preview</h5>
 				<h2>
-					<a href="${building.link}" target="_blank" style="background-color: ${building.style.color};">${building.title}</a>
+					<a href="${building.registration}" target="_blank" style="background-color: ${building.style.color};">${building.title}</a>
 				</h2>
 				<h3>${building.time}</h3>
 				<h4>${building.architect}</h4>
 				<div class="building_content">
 					<p>${building.description}</p>
-				</div><img class="building_content" src="${building.image}">
+					<p><a target="_blank" href="${building.registration}">Register</a></p>
+				</div
+				><div class="building_content">
+					<img class="" src="${building.image}">
+					<h5>${building.caption}</h5>
+				</div>
 			</li>`)
 
 	})
+
+	$("#arch_preview").append(`<li class="call_for_buildings">
+		<h2>
+			<a href="${Site.callforbuidling.link}">${Site.callforbuidling.title}</a>
+		</h2>
+		<h3>${Site.callforbuidling.deadline}</h3>
+		<img src="${Site.callforbuidling.image}">
+		<p>${Site.callforbuidling.description}</p>
+		<p><a href="${Site.callforbuidling.link}" target="_blank">Learn More</a></p>
+		</li>`)
 
 
 }
